7ef0c9eb7cb55ffdd86e041ba9f1ee860050df88
Rajinder Singh Sodhi <rajsodhisocial@gmail.com>
2024-03-14 17:48:05 +0530

    version 1

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -35,8 +35,8 @@ function Chats() {
 
   return (
     <div className='chats'>
-      {chats && Object.entries(chats).map((chat, index) => (
-        <div className='user__chats' key={index} onClick={()=>handleSelect(chat)}>
+      {chats && Object.entries(chats).map(([key, chat]) => (
+        <div className='user__chats' key={key} onClick={()=>handleSelect(chat)}>
           {chat.userInfo && (
             <>
               <img src={chat.userInfo.photoURL} alt="" />
